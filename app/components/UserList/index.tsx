59628f8a1a324246e0189583f4d63b89903225fd
hikari-tanimoto <hikari-tanimoto@gaji.jp>
2026-04-07 07:51:32 +0900

    Enhance UserList component by adding query client for refetching users and implementing stale time settings.

--- app/components/UserList/index.tsx
+++ app/components/UserList/index.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useQuery } from "@tanstack/react-query";
+import { useQuery, useQueryClient } from "@tanstack/react-query";
 
 export type UserData = {
   id: number;
@@ -14,19 +14,29 @@ const getUsers = async (): Promise<UserData[]> => {
 };
 
 export const UserList = () => {
+  const queryClient = useQueryClient();
   const { data, isLoading, isError } = useQuery<UserData[]>({
     queryKey: ["users"],
     queryFn: getUsers,
+    staleTime: 1000 * 60,
+    gcTime: 1000 * 60 * 10,
   });
 
   if (isLoading) return <div>Loading...</div>;
   if (isError || !data)
     return <div>Error: {isError && "データを取得できませんでした"}</div>;
   return (
-    <ul>
-      {data.map((user) => (
-        <li key={user.id}>{user.name}</li>
-      ))}
-    </ul>
+    <>
+      <ul>
+        {data.map((user) => (
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </ul>
+      <button
+        onClick={() => queryClient.invalidateQueries({ queryKey: ["users"] })}
+      >
+        Refetch
+      </button>
+    </>
   );
 };
